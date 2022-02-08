8f36fc2ad9410cad36763892fd2b157463473c7d
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2022-02-08 22:49:50 +0600

    Added AssertPanic

--- defs.h
+++ defs.h
@@ -198,3 +198,4 @@ void            clearpteu(pde_t *pgdir, char *uva);
 
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
+#define AssertPanic(x) if(!(x)) { cprintf("file:%s line:%d \n" , __FILE__ , __LINE__); panic(#x);}
