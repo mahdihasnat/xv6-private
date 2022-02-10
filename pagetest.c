31bff79a896b411b819f3353eacf7b9a42527ef3
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2022-02-10 23:16:56 +0600

    Added fork test 2

--- pagetest.c
+++ pagetest.c
@@ -31,21 +31,71 @@ fork_test()
 	}
 	if(x==0)
 	{
-		printf(1,"child sleep 1000\n");
-		sleep(1000);
+		printf(1,"child sleep 500\n");
+		sleep(500);
 		printf(1,"child arr[10]=%d\n",arr[10]);
 		exit();
 	}
 	else
 	{
 		sleep(100);
-		printf(1,"parent sleep 1000\n");
-		sleep(1000);
+		printf(1,"parent sleep 500\n");
+		sleep(500);
 		wait();
 		printf(1," parent arr[10]=%d\n",arr[10]);
+		sbrk(PGSIZE);
 		return ;
 	}
 }
+
+void
+fork_test2()
+{
+	int * arr =(int *) sbrk(PGSIZE);
+	int x = fork();
+	if(x<0)
+	{
+		printf(1,"fork failed\n");
+		return ;
+	}
+	int a=31;
+	if(x==0)
+	{
+		// child
+		a=1e9+7;
+	}
+	for(int i=0;i<PGSIZE/sizeof(int);i++)
+	{
+		arr[i]=a * i;
+	}
+
+	sleep(100);
+	int ok = 1;
+	for(int i=0;i<PGSIZE/sizeof(int);i++)
+	{
+		if(arr[i]!=a*i)
+		{
+			ok=0;
+			break;
+		}
+	}
+	if(x!=0)
+		sleep(100);
+	if(ok)
+	{
+		printf(1,"fork test ok\n");
+	}
+	else
+	{
+		printf(1,"fork test failed\n");
+	}
+	if(x==0)
+		exit();
+	else 
+		wait();
+
+}
+
 
 int
 main(int argc, char * argv[]){
@@ -54,6 +104,7 @@ main(int argc, char * argv[]){
 	else
 		printf(1,"fifo test failed\n");
 	fork_test();
+	fork_test2();
 	// uint curr_size=(uint)sbrk(0);
 	// printf(1,"current size = %p\n",curr_size);
 	// // ddd4
