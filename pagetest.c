64f2ecaf27dd7328b4d3bccb0dbb35f1abdc5c94
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2022-02-10 23:00:28 +0600

    Added fork test

--- pagetest.c
+++ pagetest.c
@@ -17,13 +17,43 @@ fifo_test(uint max_page)
 	return 0;
 }
 
+void
+fork_test()
+{
+	int * arr =(int *) sbrk(PGSIZE);
+	printf(1,"arr : %p\n",arr);
+	arr[10]=10;
+	printf(1,"arr[10]=%d\n",arr[10]);
+	int x = fork();
+	if(x<0)
+	{
+		printf(1,"fork failed\n");
+	}
+	if(x==0)
+	{
+		printf(1,"child sleep 1000\n");
+		sleep(1000);
+		printf(1,"child arr[10]=%d\n",arr[10]);
+		exit();
+	}
+	else
+	{
+		sleep(100);
+		printf(1,"parent sleep 1000\n");
+		sleep(1000);
+		wait();
+		printf(1," parent arr[10]=%d\n",arr[10]);
+		return ;
+	}
+}
+
 int
 main(int argc, char * argv[]){
-	if(fifo_test(30)==0)
+	if(fifo_test(0)==0)
 		printf(1,"fifo test passed\n");
 	else
 		printf(1,"fifo test failed\n");
-
+	fork_test();
 	// uint curr_size=(uint)sbrk(0);
 	// printf(1,"current size = %p\n",curr_size);
 	// // ddd4
--- vm.c
+++ vm.c
@@ -336,6 +336,8 @@ copyuvm(pde_t *pgdir, uint sz)
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
       panic("copyuvm: pte should exist");
+    if((*pte & PTE_PG))
+      continue;
     if(!(*pte & PTE_P))
       panic("copyuvm: page not present");
     pa = PTE_ADDR(*pte);
